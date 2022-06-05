f20e66f5cf4b8f95c321401bf6faa0ba664bd4c4
pauweezydesign <weezy@paulweezydesign.com>
2022-06-05 11:41:19 -0700

    Initial commit

---
+++ add/.routify/routes.default.js
@@ -0,0 +1,31 @@
+
+
+export default {
+  "meta": {},
+  "id": "_default",
+  "file": {
+    "path": "src/routes",
+    "dir": "src",
+    "base": "routes",
+    "ext": "",
+    "name": "routes"
+  },
+  "rootName": "default",
+  "routifyDir": import.meta.url,
+  "children": [
+    {
+      "meta": {},
+      "id": "_default_index_svelte",
+      "name": "index",
+      "module": () => import('../src/routes/index.svelte'),
+      "file": {
+        "path": "src/routes/index.svelte",
+        "dir": "src/routes",
+        "base": "index.svelte",
+        "ext": ".svelte",
+        "name": "index"
+      },
+      "children": []
+    }
+  ]
+}
